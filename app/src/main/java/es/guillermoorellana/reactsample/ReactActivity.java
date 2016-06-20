abfe37a4d2acb771c2377b3cbbac8b52b60dc3c5
Guillermo Orellana <guillermo.orellana@corp.badoo.com>
2016-06-20 18:18:04 +0100

    add react parts

--- app/src/main/java/es/guillermoorellana/reactsample/ReactActivity.java
+++ app/src/main/java/es/guillermoorellana/reactsample/ReactActivity.java
@@ -1,6 +1,9 @@
 package es.guillermoorellana.reactsample;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
@@ -19,15 +22,23 @@ public class ReactActivity extends AppCompatActivity implements DefaultHardwareB
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        if (Build.VERSION.SDK_INT >= 23) {
+            // Get permission to show redbox in dev builds.
+            if (!Settings.canDrawOverlays(this)) {
+                Intent serviceIntent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION);
+                startActivity(serviceIntent);
+            }
+        }
+
         mReactRootView = new ReactRootView(this);
         mReactInstanceManager = ReactInstanceManager.builder()
-                .setApplication(getApplication())
-                .setBundleAssetName("index.android.bundle")
-                .setJSMainModuleName("index.android")
-                .addPackage(new MainReactPackage())
-                .setUseDeveloperSupport(BuildConfig.DEBUG)
-                .setInitialLifecycleState(LifecycleState.RESUMED)
-                .build();
+            .setApplication(getApplication())
+            .setBundleAssetName("index.android.bundle")
+            .setJSMainModuleName("index.android")
+            .addPackage(new MainReactPackage())
+            .setUseDeveloperSupport(BuildConfig.DEBUG)
+            .setInitialLifecycleState(LifecycleState.RESUMED)
+            .build();
         mReactRootView.startReactApplication(mReactInstanceManager, "ReactSample", null);
 
         setContentView(mReactRootView);
@@ -55,7 +66,8 @@ public class ReactActivity extends AppCompatActivity implements DefaultHardwareB
     public void onBackPressed() {
         if (mReactInstanceManager != null) {
             mReactInstanceManager.onBackPressed();
-        } else {
+        }
+        else {
             super.onBackPressed();
         }
     }
---
+++ index.android.js
@@ -0,0 +1,34 @@
+'use strict';
+
+import React from 'react';
+
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+class ReactSample extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.hello}>Hello, World</Text>
+      </View>
+    )
+  }
+}
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  hello: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
+
+AppRegistry.registerComponent('ReactSample', () => ReactSample);
